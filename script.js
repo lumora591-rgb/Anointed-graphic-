1c001f8e2c9a6eeed59ba638143955c71a17c900
lumora591-rgb <lumora591@gmail.com>
2026-07-26 11:35:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,20 @@ window.addEventListener("load", () => {
     }, 1500);
 
 });
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
